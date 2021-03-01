b2d6717b673cbf2b87e404244e7896b6c96a0e3c
KennyMichael <72096449+KennyMichael@users.noreply.github.com>
2021-03-01 20:32:13 +0000

    Add files via upload

--- CA3_Scraper.py
+++ CA3_Scraper.py
@@ -2,46 +2,68 @@ import requests
 import json
 from bs4 import BeautifulSoup  # importing modules
 
-repo_url = 'https://github.com/KennyMichael/CA3-OOP'  # setting url
 
-repo = requests.get(repo_url)  # connecting to url using requests module
+def retrieve_folders():
+    repo_url = 'https://github.com/KennyMichael/CA3-OOP'  # setting url
 
-# print(repo.status_code)  # code 200 confirms we are connected
+    repo = requests.get(repo_url)  # connecting to url using requests module
 
-repo_soup = BeautifulSoup(repo.content, 'html.parser')  # Beautiful Soup repo
+    # print(repo.status_code)  # code 200 confirms we are connected
 
-# navigating github repo to links into folders using class Box-row and div/span tags
-# js-navigation tag selects only folder link not commit
-repo_script = repo_soup.select('div.Box-row div span')
-folders = []
-for week_folder in repo_script:
-    # finding links using attribute 'href'
-    href_link = week_folder.a.attrs['href']
-    folders.append(href_link)  # saving links to folders to a list
+    repo_soup = BeautifulSoup(
+        repo.content, 'html.parser')  # Beautiful Soup repo
 
-# print(folders)
+    # navigating github repo to links into folders using class Box-row and div/span tags
+    # js-navigation tag selects only folder link not commit
+    repo_script = repo_soup.select('div.Box-row div span')
+    folders = []
+    for week_folder in repo_script:
+        # finding links using attribute 'href'
+        href_link = week_folder.a.attrs['href']
+        if 'wk' in href_link:   # only downloading week folders and not the script file
+            folders.append(href_link)  # saving links to folders to a list
 
-
-# print(folders[0::2]) trying to get rid of commit hyperlinks
-
-content = {}
-for link in folders:
-
-    folder_url = 'https://github.com' + link  # urls for each of the github folders
-    # connecting to folder using the requests module
-    folder = requests.get(folder_url)
-
-    folder_soup = BeautifulSoup(
-        folder.content, 'html.parser')  # Beautiful Soup folders
-
-    folder_script = folder_soup.select('div.Box-row')
-
-    week_files = []
-    for files in folder_script:
-        href_link = files.a.attrs['href']
-        week_files.append(href_link)  # saving links to folders to a list
-        print(week_files)
-        # content[link] = week_files
+    return folders
 
 
-print(content)
+def retrieve_files(folders):  # function to take files from the folders
+    content = {}
+    for link in folders:
+
+        folder_url = 'https://github.com' + link  # urls for each of the github folders
+        # connecting to folder using the requests module
+        folder = requests.get(folder_url)
+
+        folder_soup = BeautifulSoup(
+            folder.content, 'html.parser')  # Beautiful Soup folders
+
+        folder_script = folder_soup.select('div.Box-row')
+
+        week_files = []
+        for files in folder_script:
+            href_link = files.a.attrs['href']
+            # collecting only html and pdf files
+            if href_link.lower().endswith('.pdf') or href_link.lower().endswith('.html'):
+                # saving links to folders to a list
+                week_files.append('https://github.com' + href_link)
+                content[link] = week_files
+    return(content)
+
+
+def format_content(content):
+    data = []
+    sec = content.keys()
+
+    for folder in sec:
+        # summary section puts files in html paragraph format
+        para = '<p>' + '</p><p>'.join(content[folder]) + '</p>'
+        # section number taken from last digit of key name
+        index = {'section': (folder[-1]), 'summary': para}
+        data.append(index)
+    return data
+
+
+folders = retrieve_folders()  # calling functions
+content = retrieve_files(folders)
+data = format_content(content)
+print(data)
