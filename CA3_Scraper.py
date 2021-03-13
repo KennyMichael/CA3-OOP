44ea88697f79b328243e79d20b0ad749cf26f18b
KennyMichael <72096449+KennyMichael@users.noreply.github.com>
2021-03-13 18:23:20 +0000

    Add files via upload

--- CA3_Scraper.py
+++ CA3_Scraper.py
@@ -1,5 +1,9 @@
 import requests
+from requests import get, post
 import json
+from bs4 import BeautifulSoup
+from dateutil import parser
+from datetime import datetime, timedelta
 from bs4 import BeautifulSoup  # importing modules
 
 
@@ -63,7 +67,95 @@ def format_content(content):
     return data
 
 
-folders = retrieve_folders()  # calling functions
-content = retrieve_files(folders)
-data = format_content(content)
-print(data)
+def update_moodle(data):
+    KEY = "bc7ad59923ad95f17dd955868790ccb5"
+    URL = "http://f0ae7213ef73.eu.ngrok.io/"
+    ENDPOINT = "/webservice/rest/server.php"
+
+    def rest_api_parameters(in_args, prefix='', out_dict=None):
+        """Transform dictionary/array structure to a flat dictionary, with key names
+        defining the structure.
+        Example usage:
+        >>> rest_api_parameters({'courses':[{'id':1,'name': 'course1'}]})
+        {'courses[0][id]':1,
+        'courses[0][name]':'course1'}
+        """
+        if out_dict == None:
+            out_dict = {}
+        if not type(in_args) in (list, dict):
+            out_dict[prefix] = in_args
+            return out_dict
+        if prefix == '':
+            prefix = prefix + '{0}'
+        else:
+            prefix = prefix + '[{0}]'
+        if type(in_args) == list:
+            for idx, item in enumerate(in_args):
+                rest_api_parameters(item, prefix.format(idx), out_dict)
+        elif type(in_args) == dict:
+            for key, item in in_args.items():
+                rest_api_parameters(item, prefix.format(key), out_dict)
+        return out_dict
+
+    def call(fname, **kwargs):
+        """Calls moodle API function with function name fname and keyword arguments.
+        Example:
+        >>> call_mdl_function('core_course_update_courses',
+                            courses = [{'id': 1, 'fullname': 'My favorite course'}])
+        """
+        parameters = rest_api_parameters(kwargs)
+        parameters.update(
+            {"wstoken": KEY, 'moodlewsrestformat': 'json', "wsfunction": fname})
+        # print(parameters)
+        response = post(URL+ENDPOINT, data=parameters).json()
+        if type(response) == dict and response.get('exception'):
+            raise SystemError("Error calling Moodle API\n", response)
+        return response
+
+    ################################################
+    # Rest-Api classes
+    ################################################
+
+    class LocalUpdateSections(object):
+        """Updates sectionnames. Requires: courseid and an array with sectionnumbers and sectionnames"""
+
+        def __init__(self, cid, sectionsdata):
+            self.updatesections = call(
+                'local_wsmanagesections_update_sections', courseid=cid, sections=sectionsdata)
+
+    ################################################
+    # Example
+    ################################################
+
+    # Update sections. Example for onetopic format.
+    courseid = "4"  # Exchange with valid id.
+    data = data
+    sec = LocalUpdateSections(courseid, data)
+    print(sec.updatesections)
+
+
+def retrieve_videos():
+    repo_url = 'https://drive.google.com/drive/folders/1pFHUrmpLv9gEJsvJYKxMdISuQuQsd_qX'  # setting url
+
+    repo = requests.get(repo_url)  # connecting to url using requests module
+
+    print(repo.status_code)  # code 200 confirms we are connected
+
+    repo_soup = BeautifulSoup(
+        repo.content, 'html.parser')  # Beautiful Soup repo
+
+    base = repo_soup.find('div', id="drive_main_page")
+
+    # collecting links to the videos
+    links = []
+    for item in base.find_all(attrs={"data-id": True}):
+        links.append('https://drive.google.com/file/d/' +
+                     item['data-id'] + '/view?usp=sharing')
+
+    # collecting their names
+    titles = []
+    for item in base.find_all(attrs={"data-tooltip": True}):
+        titles.append(str(item['data-tooltip']))
+    titles = titles[1:]
+
+    return links, titles
